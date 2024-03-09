01cfa97e8f1495993c2eb3845c58d79780869281
Prafull Kamal <135655190+Pkamal2002@users.noreply.github.com>
2024-03-10 00:07:26 +0530

    connect to mongoAtlas

---
+++ src/index.js
@@ -0,0 +1,51 @@
+// require('dotenv').config({path:'./env'});
+
+import dotenv from "dotenv";
+
+import connectDB from "./db/dbConnect.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+////// Pollutes the index page with database connection and express routings///
+/*
+
+import { Express } from "express";
+const app=Express();
+
+// iffi used to immidiately innvoke functions
+(async()=>{
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("error",(error)=>{
+        console.log("Error:",error)
+        throw error
+       })
+
+       app.listen(process.env.PORT,()=>{
+        console.log(`App is listening on port ${process.env.PORT}`);
+       })
+        
+    } catch (error) {
+        console.error("ERROR",error)
+        throw err
+        
+    }
+})()
+*/
