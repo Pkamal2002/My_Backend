2ad82d9d188b1b5635951d3dd59539b1977622c7
Pkamal2002 <135655190+Pkamal2002@users.noreply.github.com>
2024-03-24 13:42:34 +0530

    Add Register Controller and Bug fixes.

--- Readme.md
+++ Readme.md
@@ -2,4 +2,4 @@
 
 This is the backend with javascript consists of many js frameworks used in it.
 
-1. got error in the import in index.js i do not import the app.js file and givig error to post the routes but finally got the error and resolved it by own.
+1. got error in the import in index.js i do not import the app.js file and giving error to post the routes but finally got the error and resolved it by own.
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,7 @@
-import mongoose from "mongoose";
-import { Jwt } from "jsonwebtoken";
+import mongoose,{Schema} from "mongoose";
+import  Jwt  from "jsonwebtoken";
 import bcrypt from "bcrypt"
+
 
 const userSchema= new mongoose.Schema({
     username:{
@@ -57,7 +58,7 @@ userSchema.pre("save", async function (next){
     if(!this.isModified("password")) return next();
 
 
-    this.password=bcrypt.hash(this.password,10)
+    this.password= await bcrypt.hash(this.password,10)
     next()
 })
 
