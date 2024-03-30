35c0a49904ebc65935360e7aed1e8f88be4b1d79
Pkamal2002 <135655190+Pkamal2002@users.noreply.github.com>
2024-03-30 13:09:52 +0530

    Added user channel Profile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -270,7 +270,7 @@ const changeCurrentPassword = asyncHandler(async(req, res)=>{
 const getCurrentUser = asyncHandler(async(req, res)=>{
   return res
   .status(200)
-  .json(200, req.user, "current user fetched successfully")
+  .json( new ApiResponse(200, req.user, "current user fetched successfully"))
 })
 
 /////////UpdateAccountDetails///////////////
@@ -281,7 +281,7 @@ const updateAccountDetails = asyncHandler(async(req,res)=>{
     throw new ApiError(400, "All fields are required")
   }
 
-  const user = User.findByIdAndUpdate(
+  const user = await User.findByIdAndUpdate(
     req.user?._id,
     {
       $set: {
@@ -324,6 +324,8 @@ const updateUserAvatar = asyncHandler(async(req, res)=>{
     {new: true}
   ).select("-password")
 
+  ////////////todo: delete older Avatar
+
   return res
   .status(200)
   .json(
@@ -365,6 +367,82 @@ const updateUserCoverImage = asyncHandler(async(req, res)=>{
   )
 })
 
+////////////User Channel Profile/////////
+
+const getUserChannelProfile = asyncHandler(async(req, res) => {
+  const { username} = req.params
+
+  if (!username?.trim()) {
+    throw new ApiError( 400, "username is missing")
+  }
+
+   const channel = await User.aggregate([
+    {
+      $match:{
+        username: username?.toLowerCase()
+      }
+    },
+    {
+      $lookup:{
+        from: "subscriptions",
+        localField: _id,
+        foreignField: "channel",
+        as: "subscribers"
+      }
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: _id,
+        foreignField: "subscriber",
+        as: "subscribedTo"
+      }
+    },
+    {
+      $addFields:{
+        subscribersCount: {
+          $size: "$subscribers"
+        },
+        channelsSubscribedToCount: {
+          $size: "subscribedTo"
+        },
+        isSubscribed: {
+          $cond: {
+            if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+            then: true,
+            else: false
+          }
+        }
+      }
+    },
+    {
+      $project:{
+        fullname: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1
+
+      }
+    }
+   ])
+
+   if (!channel?.length) {
+    throw new ApiError(404, "channel does not exists")
+   }
+   
+   return res
+   .status(200)
+   .json(
+    new ApiResponse(200, channel[0], "User channel fetched successfully")
+   )
+
+})
+
+
 
 export {registerUser,
         loginUser,
@@ -374,5 +452,6 @@ export {registerUser,
         getCurrentUser,
         updateAccountDetails,
         updateUserAvatar,
-        updateUserCoverImage
+        updateUserCoverImage,
+        getUserChannelProfile
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -26,5 +26,6 @@ router.route("/login").post(loginUser)
 ///secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/updateAccount").post(updateAccountDetails)
 
 export default router
